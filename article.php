9dece60f667aef9dd32d18fb726003e34a7f1748
Shaygan Shokrollahi <shayganshok0@gmail.com>
2017-10-05 19:11:54 +0330

    Article.php edit

--- article.php
+++ article.php
@@ -1,5 +1,7 @@
 <?php
-if (empty($_GET["state"])) header('location:index.php');
+include 'app.php';
+if (empty($_GET["state"]) || !ValidState($_GET["state"])) header('location:index.php');
+$State = $_GET["state"];
 ?>
 <!DOCTYPE html>
 <html lang="fa">
@@ -18,13 +20,13 @@ if (empty($_GET["state"])) header('location:index.php');
                 <button class="sidenav-control material-icons">menu</button>
             </div>
             <div class="title">
-                استان سفید
+<?=getState($State,"name")?>
             </div>
             <ul class="sidenav">
                 <div class="sidenav-title">
                     ایران شناسی
                 </div>
-                <li><a href="index.html">
+                <li><a href="index.php">
                     خانه
                 </a></li>
             </ul>
@@ -33,8 +35,8 @@ if (empty($_GET["state"])) header('location:index.php');
     </nav>
     <div class="heroic-header" style="background-image: url(assets/dist/img/test-bg.svg)">
         <div class="content">
-            <h3>استان</h3>
-            <span>یه جمله قهرمانانه درباره استان!</span>
+            <h3><?=getState($State,"name")?></h3>
+            <span><?=getState($State,"ins_sentense")?></span>
         </div>
     </div>
     <div class="st-stats">
@@ -42,43 +44,54 @@ if (empty($_GET["state"])) header('location:index.php');
         <div class="pui-col xs-12 md-6">
             <i class="material-icons">supervisor_account</i>
             <b id="ppl_title">جمعیت</b>
-            <span id="ppl_num">n</span>
+            <span id="ppl_num"><?=getState($State,"Population")?></span>
         </div>
         <div class="pui-col xs-12 md-6">
             <i class="material-icons">location_city</i>
             <b>مرکز استان</b>
-            <span>فلان جا</span>
+            <span><?=getState($State,"city_center")?></span>
         </div>
     </div>
     <main role="main">
         <div class="pui-col xs-12 sm-10 sm-offset-1 md-8 md-offset-2">
             <div class="container">
                 <div class="row">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    <?=getState($State,"description")?>
                     <hr>
-                    <h3>مشاهیر استان</h3>
-                    <ul>
-                        <li>اصغر</li>
-                        <li>اکبر</li>
-                    </ul>
-                    <hr>
-                    <h3>مکان های دیدنی</h3>
-                    اینجا توی چند تا کارت، جاهای دیدنی رو میزاریم.
-                    <hr>
-                    <div class="images">
-                        <div class="pui-col xs-12 sm-6 md-4 lg-3">
-                            یه عکس!
-                        </div>
-                        <div class="pui-col xs-12 sm-6 md-4 lg-3">
-                            یه عکس!
-                        </div>
-                        <div class="pui-col xs-12 sm-6 md-4 lg-3">
-                            یه عکس!
-                        </div>
-                        <div class="pui-col xs-12 sm-6 md-4 lg-3">
-                            یه عکس!
-                        </div>
-                    </div>
+                    <?php
+                    $StateCode = getState($State,"n");
+                    $getHeroes = mysqli_query($db,"SELECT * FROM `Heroes` WHERE `fstate`=$StateCode");
+                    if (mysqli_num_rows($getHeroes) >= 1) {
+                        ?>
+                        <h3>مشاهیر استان</h3>
+                        <ul>
+                    <?php
+                        while ($Heroes = mysqli_fetch_assoc($getHeroes)) {
+                            echo "<li class='blue-txt' data-popover='$Heroes[description]' data-popover-place='bottom'>$Heroes[name]</li>";
+                        }
+                        ?>
+                            </ul>
+                    <?php
+                    }
+                    ?>
+<!--                    <hr>-->
+<!--                    <h3>مکان های دیدنی</h3>-->
+<!--                    اینجا توی چند تا کارت، جاهای دیدنی رو میزاریم.-->
+<!--                    <hr>-->
+<!--                    <div class="images">-->
+<!--                        <div class="pui-col xs-12 sm-6 md-4 lg-3">-->
+<!--                            یه عکس!-->
+<!--                        </div>-->
+<!--                        <div class="pui-col xs-12 sm-6 md-4 lg-3">-->
+<!--                            یه عکس!-->
+<!--                        </div>-->
+<!--                        <div class="pui-col xs-12 sm-6 md-4 lg-3">-->
+<!--                            یه عکس!-->
+<!--                        </div>-->
+<!--                        <div class="pui-col xs-12 sm-6 md-4 lg-3">-->
+<!--                            یه عکس!-->
+<!--                        </div>-->
+<!--                    </div>-->
                 </div>
             </div>
         </div>
--- functions.php
+++ functions.php
@@ -8,8 +8,19 @@ function getSetting($req) {
 }
 
 function getState($n,$req) {
+    global $db;
+    $gUser = mysqli_query($db,"SELECT * FROM `States` WHERE `name`='$n' OR `english`='$n'");
+    $User = mysqli_fetch_assoc($gUser);
+    return $User[$req];
+}
+
+function ValidState($n) {
     $db = $GLOBALS["db"];
-    $query = mysqli_query($db,"SELECT * FROM `States` WHERE `n`=$n OR `name`='$n'");
-    $result = mysqli_fetch_assoc($query);
-    return $result[$req];
+    $query = mysqli_query($db,"SELECT * FROM `States` WHERE `name`='$n' OR `english`='$n'");
+    if (mysqli_num_rows($query) == 1) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
