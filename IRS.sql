6b686cf2b181f39f0d34f30b6247fcc4290f1f80
Shaygan Shokrollahi <shayganshok0@gmail.com>
2017-10-05 19:42:51 +0330

    IRS.sql edit and add_state(hb) edit

--- IRS.sql
+++ IRS.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 05, 2017 at 02:24 PM
+-- Generation Time: Oct 05, 2017 at 04:12 PM
 -- Server version: 5.6.31
 -- PHP Version: 7.0.10
 
@@ -31,7 +31,14 @@ CREATE TABLE IF NOT EXISTS `Heroes` (
   `name` text COLLATE utf8_persian_ci NOT NULL,
   `description` text COLLATE utf8_persian_ci NOT NULL,
   `fstate` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
+
+--
+-- Dumping data for table `Heroes`
+--
+
+INSERT INTO `Heroes` (`n`, `name`, `description`, `fstate`) VALUES
+(1, 'یه خری', 'یه خری بود یکی نبود ، این خیلی خر بود ، خیلی :)', 3);
 
 -- --------------------------------------------------------
 
@@ -73,6 +80,9 @@ INSERT INTO `Setting` (`n`, `title`) VALUES
 CREATE TABLE IF NOT EXISTS `States` (
   `n` int(11) NOT NULL,
   `name` text COLLATE utf8_persian_ci NOT NULL,
+  `city_center` text COLLATE utf8_persian_ci NOT NULL,
+  `Population` bigint(20) NOT NULL,
+  `ins_sentense` text COLLATE utf8_persian_ci NOT NULL,
   `english` text COLLATE utf8_persian_ci NOT NULL,
   `description` text COLLATE utf8_persian_ci NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
@@ -81,13 +91,13 @@ CREATE TABLE IF NOT EXISTS `States` (
 -- Dumping data for table `States`
 --
 
-INSERT INTO `States` (`n`, `name`, `english`, `description`) VALUES
-(1, 'آذربایجان شرقی', 'azarbayjansharghi', ''),
-(2, 'آذربایجان غربی', 'azarbayjangharbi', ''),
-(3, 'اردبیل', 'ardebil', ''),
-(4, 'اصفهان', 'esfehan', ''),
-(5, 'ایلام', 'ilam', ''),
-(6, 'بوشهر', 'booshehr', '');
+INSERT INTO `States` (`n`, `name`, `city_center`, `Population`, `ins_sentense`, `english`, `description`) VALUES
+(1, 'آذربایجان شرقی', '', 0, '', 'azarbayjansharghi', ''),
+(2, 'آذربایجان غربی', '', 0, '', 'azarbayjangharbi', ''),
+(3, 'اردبیل', 'اردبیلمرکز', 84, 'یوهوووووووو', 'ardebil', 'شهری سفید که قدا ندارد'),
+(4, 'اصفهان', '', 0, '', 'esfehan', ''),
+(5, 'ایلام', '', 0, '', 'ilam', ''),
+(6, 'بوشهر', '', 0, '', 'booshehr', '');
 
 -- --------------------------------------------------------
 
@@ -144,7 +154,7 @@ ALTER TABLE `Wonderful_Places`
 -- AUTO_INCREMENT for table `Heroes`
 --
 ALTER TABLE `Heroes`
-  MODIFY `n` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `n` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `Pics`
 --
---
+++ admin/add_state.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="../assets/demo.css">
+</head>
+<body>
+    <div class="center-align container">
+        <h1 style="font-weight: 200">اضافه کردن استان</h1>
+        <form action="" method="post">
+            <div style="max-width: 300px; width: 100%; margin:0 auto">
+                <div class="pui-input">
+                    <input type="text" name='name' placeholder="نام استان">
+                </div>
+                <div class="pui-input">
+                    <input type="text" name='english' placeholder="فینگیلیش...">
+                </div>
+                <div class="pui-input">
+                    <input type="text" name='ins_sentense' placeholder="جمله قهرمانانه">
+                </div>
+                <div class="pui-input">
+                    <input type="number" min="0" name='population' placeholder="جمعیت">
+                </div>
+                <div class="pui-input">
+                    <input type="text" name='city_center' placeholder="مرکز استان">
+                </div>
+            </div>
+            <div class="pui-textarea pui-col xs-12 md-8 md-offset-2">
+                <textarea name="description" rows="8" cols="80" placeholder="پاراگراف..."></textarea>
+            </div>
+            <div class="cf"></div>
+            <div style="margin: 0 auto" class="center-align">
+                <button type="submit" name="submit" class="btn secondary lg"><i class="material-icons">done</i> ثبت استان</button>
+            </div>
+            <?php
+            if (isset($_POST["submit"])) {
+                $name = $_POST["name"];
+                $english = $_POST["english"];
+                $ins_sentense = $_POST["ins_sentense"];
+                $population = $_POST["population"];
+                $city_center = $_POST["city_center"];
+                $description = $_POST["description"];
+                $addState = mysqli_query($db,"INSERT INTO `States` (`name`,city_center,Population,ins_sentense,description) VALUES ('$name','$city_center',$population,'$ins_sentense','$description')");
+                if ($addState) {
+                    echo '<div class="chip">موفقیت آمیز بود :)</div>';
+                }
+                else {
+                    echo '<div class="chip">موفقیت آمیز نبود :(</div>';
+                }
+            }
+            ?>
+        </form>
+    </div>
+</body>
+<script src="../assets/dist/jquery-3.1.1.min.js"></script>
+<script src="../assets/dist/pui.min.js"></script>
+</html>
