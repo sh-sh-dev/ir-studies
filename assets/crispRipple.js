a8f24f8799ff10820532c40629f5fe9f23956b32
hkh12 <hrk.x11@gmail.com>
2017-10-07 21:45:19 +0330

    crisp ripple!

---
+++ assets/crispRipple.js
@@ -0,0 +1,34 @@
+(function($) {
+    'use strict';
+    var el = $('.home-header');
+    var color = el.css('background-color');
+    el.attr('data-crr-hold', 0);
+    var data = el.data('crr-hold');
+    el.append('<div class="crisp-container" />').click(function () {
+        data++;
+        el.attr('data-crr-hold', data);
+    }).click(function (e) {
+        var pos = {
+            x: e.pageX - el.offset().left,
+            y: e.pageY - el.offset().top
+        },
+        container = el.find('.crisp-container'),
+        crisp = $('<div class="crisp-ripple" />'),
+        size = Math.max( el.height(), el.width() ) * Math.PI;
+        var bg = data%2 == 0 || data == 0 ? color : '#888';
+        crisp.appendTo(container).css({
+            'left': pos.x,
+            'top': pos.y,
+            'background-color': bg
+        })
+        setTimeout(function () {
+            crisp.css({
+                'height': size,
+                'width': size
+            }).on('transitionend', function () {
+                el.css('background-color', crisp.css('background-color') );
+                crisp.remove()
+            })
+        }, 1);
+    })
+}(jQuery));
