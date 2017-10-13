8abf1f42740a821892c4eaff77a20c3dc2801421
Shaygan Shokrollahi <shayganshok0@gmail.com>
2017-10-13 22:20:43 +0330

    add error/403

---
+++ error/403.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>شما اجازه دسترسی به این صفحه را ندارید!</title>
+    <meta name="theme-color" content="#aeea00">
+    <meta name="msapplication-navbutton-color" content="#aeea00">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#aeea00">
+    <link rel="stylesheet" href="../assets/irs.css">
+</head>
+<body id="page-404">
+<div class="flex-centered" style="height: 100vh">
+    <section>
+        <i class="material-icons p404-icon">visibility_off</i>
+        <span class="p404-title">error 403</span>
+        <span style="font-weight: bold">متاسفیم، شما اجازه دسترسی به این صفحه را ندارید!</span>
+        <br><br>
+        <a href=".." class="btn primary fab material-icons">home</a>
+    </section>
+</div>
+</body>
+<script src="../assets/dist/jquery.min.js"></script>
+<script src="../assets/dist/pui.min.js"></script>
+<script src="../assets/irs.min.js"></script>
+</html>
