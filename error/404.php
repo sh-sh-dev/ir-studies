ab1323cf0a51c4da003eada1f932c519c94da7b9
hkh12 <hrk.x11@gmail.com>
2017-10-14 18:34:59 +0330

    little changes in 404 & 403

--- error/404.php
+++ error/404.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>صفحه مورد نظر پیدا نشد!</title>
-    <meta name="theme-color" content="#aeea00">
-    <meta name="msapplication-navbutton-color" content="#aeea00">
-    <meta name="apple-mobile-web-app-status-bar-style" content="#aeea00">
+    <meta name="theme-color" content="#546e7a">
+    <meta name="msapplication-navbutton-color" content="#546e7a">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#546e7a">
     <link rel="stylesheet" href="../assets/irs.css">
 </head>
 <body id="page-404">
@@ -15,9 +15,11 @@
         <section>
             <i class="material-icons p404-icon">cloud_off</i>
             <span class="p404-title">error 404</span>
+            <br>
             <span style="font-weight: bold">اوه، مثل اینکه این صفحه وجود نداره!</span>
             <br><br>
-            <a href=".." class="btn primary fab material-icons">home</a>
+            <a href=".." class="btn primary fab material-icons" id="back_to_home"
+            data-tooltip='بازگشت به خانه' data-tooltip-container='#back_to_home'>home</a>
         </section>
     </div>
 </body>
