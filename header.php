8f6fbf5ad2f11a6fa0ab0419923595b721e2eff3
hkh12 <hrk.x11@gmail.com>
2017-10-14 18:20:17 +0330

    state 5 - palette and theme-color meta

--- header.php
+++ header.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?=getSetting('title')?></title>
-    <meta name="theme-color" content="#aeea00">
-    <meta name="msapplication-navbutton-color" content="#aeea00">
-    <meta name="apple-mobile-web-app-status-bar-style" content="#aeea00">
+    <meta name="theme-color" content="#546e7a">
+    <meta name="msapplication-navbutton-color" content="#546e7a">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#546e7a">
     <link rel="stylesheet" href="<?=getSetting('url')?>/assets/irs.css">
     <script src="<?=getSetting('url')?>/assets/dist/jquery.min.js"></script>
 </head>
