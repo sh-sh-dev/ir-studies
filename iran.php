749925642244afa36dffe768bd50428df9fb22cf
Shaygan Shokrollahi <shayganshok0@gmail.com>
2017-10-18 22:16:36 +0330

    database delete some files tdo.txt edit

--- iran.php
+++ iran.php
@@ -1,23 +1,23 @@
 <?php
 include 'app.php';
 ?>
-<?php getHeader() ?>
+<?getHeader()?>
 <body>
     <div class="ui">
         <div class="header-box">
             <?php getMenu() ?>
         </div>
         <div class="box-container">
-            <!-- <div class="audio-player">
+            <div class="audio-player">
                 <audio id='au'>
                     <source src="assets/dist/anthem.mp3">
                 </audio>
                 <div class="controls">
                     <div class="slider big-slider"></div>
                 </div>
-            </div> -->
+            </div>
         </div>
     </div>
 </body>
 <?=setTitle('ایران', 1)?>
-<?php getFooter(); ?>
+<?getFooter(); ?>
