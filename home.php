da9b00567b6ba1f0913457cbb68233baf06af7a1
hkh12 <hrx.x11@gmail.com>
2017-10-29 16:39:07 +0330

    triggered again

--- home.php
+++ home.php
@@ -8,7 +8,7 @@ include 'app.php';
             <?getMenu()?>
         </div>
         <div class="box-container">
-            <div class="has-crr home-header" data-crr-color='#ffc107|#263238'>
+            <div class="has-crr home-header" data-crr-color='#009688|#263238'>
                 <div class="content">
                     <h1>ایران شناسی</h1>
                 </div>
@@ -92,39 +92,6 @@ include 'app.php';
             </div>
         </div>
     </div>
-    <!-- <div class="container">
-    <div class="row">
-    <div class="space-80"></div>
-    <div class="pui-col xs-12 center-align">
-    <h3><?//=getSetting('title')?></h3>
-    <div style="max-width: 280px; width: 100%; margin: 0 auto">
-    <form action="" method="post">
-    <div class="pui-datalist">
-    <div class="pui-input">
-    <input type="text" id="s" name="state_search" placeholder="استان مورد نظر خود را جستجو کنید...">
-</div>
-</div>
-</form>
-</div>
-</div>
-<div class="space-80"></div>
-</div>
-</div> -->
-<script type="text/javascript">
-function DataList() {
-    <?php
-    $query = mysqli_query($db,"SELECT `name` FROM `States` WHERE `active`=1");
-    $json = array();
-    while ($States = mysqli_fetch_assoc($query)) {
-        array_push($json,$States["name"]);
-    }
-    $json = json_encode($json);
-    ?>
-    $('#s').dataList(
-        <?=$json?>
-    )
-}
-</script>
 <?=setTitle("خانه", 1)?>
 <script src="assets/crispRipple.min.js"></script>
 <?getFooter()?>
