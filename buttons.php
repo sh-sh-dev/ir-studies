e72722e20dc7db58d84da1ca455851519ef5cf7b
Hkh12 <hrk.x11@gmail.com>
2017-11-16 14:49:49 +0330

    buttons

---
+++ buttons.php
@@ -0,0 +1,5 @@
+<div class="buttons">
+    <a class="material-icons has-ripple">arrow_back</a>
+    <a class="material-icons has-ripple">home</a>
+    <a class="material-icons has-ripple">menu</a>
+</div>
