6ae74ecd01ff1cf367e1ade8997cc75770ef4d2f
Hkh12 <hrk.x11@gmail.com>
2017-11-17 22:15:53 +0330

    Ah, Merge again

--- buttons.php
+++ buttons.php
@@ -1,15 +1,4 @@
 <div class="buttons">
-<!--
-    <a class="has-ripple">
-        <img src="assets/dist/img/buttons/icon_back.svg">
-    </a>
-    <a class="has-ripple">
-        <img src="assets/dist/img/buttons/icon_home.svg">
-	</a>
-    <a class="has-ripple" href="javascript:$('.sidenav').addClass('active')">
-        <img src="assets/dist/img/buttons/icon_navigation.svg">
-    </a>
--->
     <?php
     if (!empty($_SERVER['HTTP_REFERER'])) {
         ?>
@@ -19,7 +8,9 @@
     <? }
     else {
         ?>
-<!--    <a class="-has-ripple">arrow_back</a>-->
+			<a class="has-ripple">
+				<img src="assets/dist/img/buttons/icon_back.svg">
+			</a>
         <?php
     }
     ?>
